e8017220d056ae826e9745c090f51fae2feabf00
Tony <antonylbrussolo@gmail.com>
2023-01-25 19:34:44 -0500

    home page banner + nav bar

---
+++ src/components/layout.tsx
@@ -0,0 +1,11 @@
+import Navbar from './nav'
+import React from "react";
+
+export default function Layout({ children }: { children: React.ReactElement }) {
+  return (
+    <div className="overflow-hidden">
+      <Navbar />
+      <main>{children}</main>
+    </div>
+  )
+}
