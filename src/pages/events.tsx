3eb086dfe464721f2c61130c74e227480ac847ca
aidonmccomb <101207078+aidonmccomb@users.noreply.github.com>
2023-01-29 05:58:42 -0500

    events.tsx has content now

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,17 +1,57 @@
-import React from "react";
-import { type NextPage } from "next";
-import { api } from "../utils/api";
+/* This example requires Tailwind CSS v2.0+ */
+import { PaperClipIcon } from '@heroicons/react/20/solid'
 import Layout from "../components/layout";
 
 
-const Events: NextPage = () => {
-  const { data, isLoading } = api.events.getEvent.useQuery();
-  return (
-    <Layout>
-      <div className="bg-gradient-to-b from-[#1e3a8a] to-[#312e81]">
-      </div>
-    </Layout>
-  );
-};
-
-export default Events;
+export default function Example() {
+    return (
+        <Layout>
+            <div className="m-10 border-2">
+        <div className="overflow-hidden bg-white shadow sm:rounded-lg">
+            <div className="px-4 py-5 sm:px-6">
+                <h3 className="text-lg font-medium leading-6 text-gray-900">Event Title</h3>
+                <p className="mt-1 max-w-2xl text-sm text-gray-500">Event Line number two.</p>
+            </div>
+            <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
+                <dl className="sm:divide-y sm:divide-gray-200">
+                    <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">Start Date</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Date 1</dd>
+                    </div>
+                    <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">End Date</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Date 2</dd>
+                    </div>
+                    <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">Description</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                            Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur
+                            qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud
+                            pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
+                        </dd>
+                    </div>
+                    <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">Attachments</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                            <ul role="list" className="divide-y divide-gray-200 rounded-md border border-gray-200">
+                                <li className="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
+                                    <div className="flex w-0 flex-1 items-center">
+                                        <PaperClipIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                                        <span className="ml-2 w-0 flex-1 truncate">event_package_one.pdf</span>
+                                    </div>
+                                    <div className="ml-4 flex-shrink-0">
+                                        <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                            Download
+                                        </a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </dd>
+                    </div>
+                </dl>
+            </div>
+        </div>
+            </div>
+        </Layout>
+    )
+}
