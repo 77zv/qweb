e123272fddf4069a1a37315b6e15aa1bf8f21be2
Tony <antonylbrussolo@gmail.com>
2023-01-29 01:50:47 -0500

    upates 🥶🥶

--- src/pages/admin-panel.tsx
+++ src/pages/admin-panel.tsx
@@ -19,7 +19,7 @@ interface Person {
 const AdminPanel: NextPage = () => {
   return (
     <Layout>
-      <div className="mr-10 ml-10">
+      <div className="mr-64 ml-64">
         <form className="space-y-8 divide-y divide-gray-200">
           <div className="space-y-8 divide-y divide-gray-200">
             <div>
@@ -61,6 +61,21 @@ const AdminPanel: NextPage = () => {
                   <p className="mt-2 text-sm text-gray-500">Write a description for the event.</p>
                 </div>
 
+                {/*date stuff*/}
+                <div className="sm:col-span-4">
+                  <label htmlFor="title" className="block text-sm font-medium text-gray-700">
+                    Submissions Open
+                  </label>
+                  <div className="mt-1 flex rounded-md rounded-md shadow-sm">
+                    <input
+                      type="datetime-local"
+                      name="title"
+                      id="title"
+                      className="block w-full min-w-0 flex-1 rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
                 <div className="sm:col-span-6">
                   <label htmlFor="cover-photo" className="block text-sm font-medium text-gray-700">
                     Cover photo
@@ -96,11 +111,21 @@ const AdminPanel: NextPage = () => {
                     </div>
                   </div>
                 </div>
+                <div className="block w-full min-w-0 flex-1 rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                  <Users/>
+                </div>
               </div>
             </div>
           </div>
+          <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+            <button
+              type="submit"
+              className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              Save
+            </button>
+          </div>
         </form>
-        <Users></Users>
       </div>
     </Layout>
   );
@@ -117,7 +142,7 @@ const Users: React.FC = () => {
 
   return (
     <Combobox as="div" value={selectedPerson} onChange={setSelectedPerson}>
-      <Combobox.Label className="block text-sm font-medium text-gray-700">Assigned to</Combobox.Label>
+      <Combobox.Label className="block text-sm font-medium text-gray-700">Judges</Combobox.Label>
       <div className="relative mt-1">
         <Combobox.Input
           className="w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
@@ -166,7 +191,6 @@ const Users: React.FC = () => {
     </Combobox>
   );
 };
-
 
 export default AdminPanel;
 
