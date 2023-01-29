ab8243f0021dec93a15d45f59cadb1dde8a6c360
aidonmccomb <101207078+aidonmccomb@users.noreply.github.com>
2023-01-28 21:15:01 -0500

    about-us.tsx made contact us filled with template

---
+++ src/pages/about-us.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from "../components/layout";
+
+const MyComponent = () => {
+    return (
+        <Layout>
+            <div>
+                <text>"Placeholder"</text>
+            </div>
+        </Layout>
+    );
+};
+
+export default MyComponent;
