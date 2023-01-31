a791ee337ccbea4366ff8e5503da8e29605a145a
matt <30198937+6mp@users.noreply.github.com>
2023-01-31 17:49:44 -0500

    upload to s3 bucket

--- src/server/api/routers/events.ts
+++ src/server/api/routers/events.ts
@@ -1,5 +1,11 @@
 import { z } from "zod";
 import { adminProcedure, createTRPCRouter, publicProcedure } from "../trpc";
+import {
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export const eventRouter = createTRPCRouter({
   getEvent: publicProcedure.query(async ({ ctx }) => {
@@ -15,73 +21,58 @@ export const eventRouter = createTRPCRouter({
       console.log(error);
     }
   }),
-
-  upsertEvent: adminProcedure.input(
-    z.object({
-      id: z.string(),
-      title: z.string(),
-      description: z.string(),
-      file: z.instanceof(File),
-      submissionsOpen: z.optional(z.date()),
-      submissionsClose: z.optional(z.date())
-    })).mutation(async ({ ctx, input }) => {
-    const {
-      id,
-      title,
-      description,
-      file,
-      submissionsOpen,
-      submissionsClose
-    } = input;
-    try {
-
-      // upload file to r2
-
-      return await ctx.prisma.event.upsert({
-        where: {
-          id: id
-        },
-        create: {
-          title,
-          description,
-          fileUrl: "https://www.google.com",
-          submissionsOpen,
-          submissionsClose
-        },
-        update: {
-          title,
-          description,
-          submissionsOpen,
-          submissionsClose
-        }
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  }),
-/*
   updateEvent: adminProcedure.input(
     z.object({
       id: z.string(),
       title: z.string(),
       description: z.string(),
+      file: z.optional(z.instanceof(File)),
       submissionsOpen: z.optional(z.date()),
       submissionsClose: z.optional(z.date())
     })
   ).mutation(async ({ ctx, input }) => {
-    const { id, title, description, submissionsOpen, submissionsClose } = input;
+    const { id, title, description, file, submissionsOpen, submissionsClose } = input;
     try {
-      return await ctx.prisma.event.update({
-        where: {
-          id: id
-        },
-        data: {
-          title,
-          description,
-          submissionsOpen,
-          submissionsClose
-        }
-      });
+      // check if we need to upload file to r2, if so update fileUrl also
+      if (file) {
+        // upload to r2
+        const r2UploadResp = await ctx.r2.send(new PutObjectCommand({
+          Bucket: "qweb",
+          Key: file.name,
+          Body: file
+        }));
+
+        // expires in one year
+        const fileUrl = await getSignedUrl(ctx.r2, new GetObjectCommand({
+          Bucket: "qweb",
+          Key: file.name
+        }), { expiresIn: 31536000 });
+
+        return await ctx.prisma.event.update({
+          where: {
+            id: id
+          },
+          data: {
+            title,
+            description,
+            fileUrl,
+            submissionsOpen,
+            submissionsClose
+          }
+        });
+      } else {
+        return await ctx.prisma.event.update({
+          where: {
+            id: id
+          },
+          data: {
+            title,
+            description,
+            submissionsOpen,
+            submissionsClose
+          }
+        });
+      }
     } catch (error) {
       console.log(error);
     }
@@ -90,16 +81,38 @@ export const eventRouter = createTRPCRouter({
     z.object({
       title: z.string(),
       description: z.string(),
+      file: z.instanceof(File),
       submissionsOpen: z.optional(z.date()),
       submissionsClose: z.optional(z.date())
     })
   ).mutation(async ({ ctx, input }) => {
-    const { title, description, submissionsOpen, submissionsClose } = input;
+    const {
+      title,
+      description,
+      file,
+      submissionsOpen,
+      submissionsClose
+    } = input;
     try {
+      // upload to r2
+      const r2UploadResp = await ctx.r2.send(new PutObjectCommand({
+        Bucket: "qweb",
+        Key: file.name,
+        Body: file
+      }));
+
+      // expires in one year
+      const fileUrl = await getSignedUrl(ctx.r2, new GetObjectCommand({
+        Bucket: "qweb",
+        Key: file.name
+      }), { expiresIn: 31536000 });
+
+
       return await ctx.prisma.event.create({
         data: {
           title,
           description,
+          fileUrl,
           submissionsOpen,
           submissionsClose
         }
@@ -108,5 +121,5 @@ export const eventRouter = createTRPCRouter({
       console.log(error);
     }
   })
-  */
+
 });
