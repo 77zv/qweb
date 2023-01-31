8b256e45ef308e28d2e4aa803d730df18992123f
owen <21ojg3@queensu.ca>
2023-01-31 01:01:40 -0500

    "minor" formatting

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,16 +1,16 @@
 import { type DefaultSession, type DefaultUser } from "next-auth";
 
 declare module "next-auth" {
-  /**
-   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-   */
+    /**
+     * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+     */
 
-  interface Session {
-    user?: User & DefaultSession["user"];
-  }
+    interface Session {
+        user?: User & DefaultSession["user"];
+    }
 
-  interface User extends DefaultUser {
-    id: string;
-    role: string;
-  }
+    interface User extends DefaultUser {
+        id: string;
+        role: string;
+    }
 }
