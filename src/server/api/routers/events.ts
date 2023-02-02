8a2a8c56bb59db8c92d5685c1747b45d632d3c35
matt <30198937+6mp@users.noreply.github.com>
2023-02-01 20:26:35 -0500

    remove log

--- src/server/api/routers/events.ts
+++ src/server/api/routers/events.ts
@@ -148,7 +148,6 @@ export const eventRouter = createTRPCRouter({
         )
         .mutation(async ({ ctx, input }) => {
             const { id, title, description, file, submissionsOpen, submissionsClose } = input;
-            console.log(input);
             try {
                 let fileUrl: string | undefined = undefined;
                 if (file) {
