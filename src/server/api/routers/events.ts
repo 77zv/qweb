265e308dfd5dd60afa70db44fe362e6f0a2130fc
Tony <antonylbrussolo@gmail.com>
2023-02-17 22:00:04 -0500

    cors is broken right now fuck

--- src/server/api/routers/events.ts
+++ src/server/api/routers/events.ts
@@ -5,81 +5,81 @@ import { env } from "../../../env/server.mjs";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export const eventRouter = createTRPCRouter({
-  getEvent: publicProcedure.query(async ({ ctx }) => {
-    try {
-      return await ctx.prisma.event.findFirst();
-    } catch (error) {
-      console.log(error);
-    }
-  }),
-  upsertEvent: adminProcedure
-    .input(
-      z.object({
-        id: z.string(),
-        title: z.string(),
-        description: z.string(),
-        file: z.optional(
-          z.object({
-            name: z.string(),
-            filetype: z.string()
-          })
-        ),
-        submissionsOpen: z.optional(z.date()),
-        submissionsClose: z.optional(z.date())
-      })
-    )
-    .mutation(async ({ ctx, input }) => {
-      const {
-        id,
-        title,
-        description,
-        file,
-        submissionsOpen,
-        submissionsClose
-      } = input;
-      try {
-        let fileUrl: string | undefined = undefined;
-        let signedUrl: string = "";
-        if (file) {
-          const command = new PutObjectCommand({
-            Bucket: "qweb",
-            Key: file.name,
-            ContentType: file.filetype,
-            Body: "BODY"
-          });
-
-          signedUrl = await getSignedUrl(ctx.r2, command, {
-            expiresIn: 3600,
-          });
-
-          fileUrl = env.S3_PUBLIC_URL + file.name;
+    getEvent: publicProcedure.query(async ({ ctx }) => {
+        try {
+            return await ctx.prisma.event.findFirst();
+        } catch (error) {
+            console.log(error);
         }
+    }),
+    upsertEvent: adminProcedure
+      .input(
+        z.object({
+            id: z.string(),
+            title: z.string(),
+            description: z.string(),
+            file: z.optional(
+              z.object({
+                  name: z.string(),
+                  filetype: z.string()
+              })
+            ),
+            submissionsOpen: z.optional(z.date()),
+            submissionsClose: z.optional(z.date())
+        })
+      )
+      .mutation(async ({ ctx, input }) : Promise<string> => {
+          const {
+              id,
+              title,
+              description,
+              file,
+              submissionsOpen,
+              submissionsClose
+          } = input;
+          try {
+              let fileUrl: string | undefined = undefined;
+              let signedUrl = ""
+              if (file) {
+                  const command = new PutObjectCommand({
+                      Bucket: "qweb",
+                      Key: file.name,
+                      ContentType: file.filetype,
+                      Body: "BODY"
+                  });
 
-        await ctx.prisma.event.upsert({
-          where: {
-            id
-          },
-          create: {
-            title,
-            description,
-            fileUrl,
-            submissionsOpen,
-            submissionsClose
-          },
-          update: {
-            title,
-            description,
-            fileUrl,
-            submissionsOpen,
-            submissionsClose
+                  signedUrl = await getSignedUrl(ctx.r2, command, {
+                      expiresIn: 3600
+                  });
+
+                  fileUrl = env.S3_PUBLIC_URL + file.name;
+              }
+
+              await ctx.prisma.event.upsert({
+                  where: {
+                      id
+                  },
+                  create: {
+                      title,
+                      description,
+                      fileUrl,
+                      submissionsOpen,
+                      submissionsClose
+                  },
+                  update: {
+                      title,
+                      description,
+                      fileUrl,
+                      submissionsOpen,
+                      submissionsClose
+                  }
+              });
+
+              return signedUrl;
+
+          } catch (error) {
+              console.log(error);
+              return "fuck this";
           }
-        });
-
-        return signedUrl;
-
-      } catch (error) {
-        return "fuck this";
-        console.log(error);
-      }
-    })
+      })
 });
