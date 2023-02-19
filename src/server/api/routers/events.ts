1a6eddf17dd39f06c9b7041f0733456e362f9c38
Tony <antonylbrussolo@gmail.com>
2023-02-18 19:30:11 -0500

    createPresignedUrl

--- src/server/api/routers/events.ts
+++ src/server/api/routers/events.ts
@@ -2,6 +2,8 @@ import { z } from "zod";
 import { adminProcedure, createTRPCRouter, publicProcedure } from "../trpc";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { env } from "../../../env/server.mjs";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
 
 export const eventRouter = createTRPCRouter({
     getEvent: publicProcedure.query(async ({ ctx }) => {
@@ -12,60 +14,74 @@ export const eventRouter = createTRPCRouter({
         }
     }),
     upsertEvent: adminProcedure
-        .input(
-            z.object({
-                id: z.string(),
-                title: z.string(),
-                description: z.string(),
-                file: z.optional(
-                    z.object({
-                        name: z.string(),
-                        body: z.string(),
+      .input(
+        z.object({
+            id: z.string(),
+            title: z.string(),
+            description: z.string(),
+            file: z.optional(
+              z.object({
+                  name: z.string(),
+                  body: z.string()
+              })
+            ),
+            submissionsOpen: z.optional(z.date()),
+            submissionsClose: z.optional(z.date())
+        })
+      )
+      .mutation(async ({ ctx, input }) => {
+          const { id, title, description, file, submissionsOpen, submissionsClose } = input;
+          try {
+              let fileUrl: string | undefined = undefined;
+              if (file) {
+                  // upload to r2
+                  await ctx.r2.send(
+                    new PutObjectCommand({
+                        Bucket: "qweb",
+                        Key: file.name,
+                        Body: file.body,
+                        ContentType: "application/pdf"
                     })
-                ),
-                submissionsOpen: z.optional(z.date()),
-                submissionsClose: z.optional(z.date()),
+                  );
+
+                  fileUrl = env.S3_PUBLIC_URL + file.name;
+              }
+
+              return await ctx.prisma.event.upsert({
+                  where: {
+                      id
+                  },
+                  create: {
+                      title,
+                      description,
+                      fileUrl,
+                      submissionsOpen,
+                      submissionsClose
+                  },
+                  update: {
+                      title,
+                      description,
+                      fileUrl,
+                      submissionsOpen,
+                      submissionsClose
+                  }
+              });
+          } catch (error) {
+              console.log(error);
+          }
+      }),
+    createPresignedUrls: adminProcedure
+      .query(async ({ ctx }) => {
+          const key = crypto.randomUUID();
+
+          const url = await getSignedUrl(
+            ctx.r2,
+            new PutObjectCommand({
+                Bucket: "qweb",
+                Key: key
             })
-        )
-        .mutation(async ({ ctx, input }) => {
-            const { id, title, description, file, submissionsOpen, submissionsClose } = input;
-            try {
-                let fileUrl: string | undefined = undefined;
-                if (file) {
-                    // upload to r2
-                    await ctx.r2.send(
-                        new PutObjectCommand({
-                            Bucket: "qweb",
-                            Key: file.name,
-                            Body: file.body,
-                            ContentType: "application/pdf"
-                        })
-                    );
+          );
 
-                    fileUrl = env.S3_PUBLIC_URL + file.name;
-                }
-
-                return await ctx.prisma.event.upsert({
-                    where: {
-                        id,
-                    },
-                    create: {
-                        title,
-                        description,
-                        fileUrl,
-                        submissionsOpen,
-                        submissionsClose,
-                    },
-                    update: {
-                        title,
-                        description,
-                        fileUrl,
-                        submissionsOpen,
-                        submissionsClose,
-                    },
-                });
-            } catch (error) {
-                console.log(error);
-            }
-        }),
+          return { key, url };
+      })
 });
